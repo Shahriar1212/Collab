a45ce4a5b6a527fbbb81481dd13a28b5243553fc
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2020-09-06 12:20:33 +0600

    first

---
+++ Collab_Profile/app/Research.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Research extends Model
+{
+    protected $guarded = [];
+    // protected $fillable = [
+    //     'title', 'user_id', 'description',
+    // ];
+}
