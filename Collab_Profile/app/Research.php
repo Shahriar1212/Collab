19f6f341a53cc5baeef9e9c821e848f8e9c3519c
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-08 21:47:29 +0600

    profile issue gone

--- Collab_Profile/app/Research.php
+++ Collab_Profile/app/Research.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Research extends Model
 {
+    protected $table = 'researches';
     protected $guarded = [];
     // protected $fillable = [
     //     'title', 'user_id', 'description',
