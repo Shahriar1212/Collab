19c067e2b94975d97814ee6e0a88e5b1f6d7f3c6
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-10 23:26:59 +0600

    Interest handled in frontend

--- Collab_Profile/public/js/handle-interest.js
+++ Collab_Profile/public/js/handle-interest.js
@@ -29,26 +29,67 @@ $(".newInterestChildWrappers li").click(function(){
     var each_data = $(this).text()
     selectedInterest.push(each_data)
 
-    // console.log($(this).text())
-
 })
 
 $(".send_my_interest").click(function(e){
     console.log(selectedInterest)
+    if(selectedInterest.length == 0){
+        alert("Please Select an Interest");
+        location.reload();
+        return false;
+    }else{
+        var usr_id = $(".get_usr_id").text()
+        var selectedInterests = `
+            {
+                "user_id": ${usr_id},
+                "data": ${JSON.stringify(selectedInterest)}
+            }    
+         `
+        $.ajax({
+            url: "/api/interest/post",
+            method: "POST",
+            data: selectedInterests,
+            dataType: "json",
+            success: function(){
+            console.log("Success")
+            }
+        });
+    }
+    
+})
+
+let user_id = $(".get_usr_id").text();
+
+$(document).ready(function() {
     var usr_id = $(".get_usr_id").text()
-    var selectedInterests = `
-        {
-            "user_id": ${usr_id},
-            "data": ${JSON.stringify(selectedInterest)}
-        }    
-    `
     $.ajax({
-        url: "/api/interest/post",
-        method: "POST",
-        data: selectedInterests,
-        dataType: "json",
-        success: function(){
-          console.log("Success")
+        url: "/api/interest/get/"+usr_id,
+        method: "GET",
+        success: function(data){
+            manipulateData(data);
         }
     });
-})
+});
+
+var got_interest_arr=[];
+
+function manipulateData(data){
+    json_object = JSON.parse(data);
+    
+    json_object[0].forEach(element => {
+        var itm = element['interest'];
+        got_interest_arr.push(itm);
+    });
+    $(".newInterestChildWrappers li").each(function(i){
+        for(var i = 0 ; i < got_interest_arr.length ;i++){
+            if($(this).text() == got_interest_arr[i]){
+                $(this).css("background-color","white");
+                $(this).css("color","black");
+                $(this).css("pointer-events","none");
+            }
+        }
+    });
+}
+
+
+
