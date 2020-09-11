89ee9dccdd7e3af8287e164ad023d8dec58c6c6f
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-11 21:00:26 +0600

    Navbar issue solved...

--- Collab_Profile/resources/views/welcome.blade.php
+++ Collab_Profile/resources/views/welcome.blade.php
@@ -20,11 +20,11 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="{{route('signup')}}">Collab.</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse navbar__menu" id="navbarNav">
+                </button> -->
+                <div>
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
                             <a class="nav-link navbar__menu--login" href="{{route('login_home')}}">Log in</a>
